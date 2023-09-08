2936d5a1a4dd2c9e83258bd833e1269f945eae3b
seung-woo-ryu <tmddn645@naver.com>
2023-09-08 20:49:16 +0900

    init

---
+++ src/main/java/com/potenday/kreamish/KreamishApplication.java
@@ -0,0 +1,13 @@
+package com.potenday.kreamish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreamishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreamishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potenday/kreamish/KreamishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potenday.kreamish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KreamishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
