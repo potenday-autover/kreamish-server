36ee1fdc85ec1e22f0fdc25bb11f8993ca8e1809
kangyunsik <re12io@ajou.ac.kr>
2023-09-11 20:44:22 +0900

    간단한 test코드 작성

---
+++ src/test/java/com/potenday/kreamish/repository/ItemRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.potenday.kreamish.repository;
+
+import com.potenday.kreamish.entity.Item;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class ItemRepositoryTest {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Test
+    void simpleSpringDataJpaTest() {
+        //given
+        String itemName = "item1";
+        Item item = new Item();
+        item.setName(itemName);
+
+        itemRepository.save(item);
+
+        //when
+        Item findItem = itemRepository.findById(item.getItemId())
+                .orElseThrow();
+
+        //then
+        assertThat(findItem.getName()).isEqualTo(itemName);
+    }
+}
