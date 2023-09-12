3f3132fb1d67f02a2cdbd32ac23df7487292e7bf
seung-woo-ryu <tmddn645@naver.com>
2023-09-12 21:57:52 +0900

    add: ApiUtil class, 모든 카테고리 리스트 반환 api

---
+++ src/main/java/com/potenday/kreamish/category/dto/CategoriesDto.java
@@ -0,0 +1,30 @@
+package com.potenday.kreamish.category.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kreamish.kream.category.entity.Category;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@ToString
+public class CategoriesDto {
+
+    @JsonProperty(value = "categories")
+    List<CategoryDto> categoryDtoList;
+
+    public static CategoriesDto of(List<Category> categories) {
+        return new CategoriesDto(categories.stream()
+            .map(CategoryDto::of)
+            .collect(Collectors.toList()));
+    }
+
+    public boolean isEmpty() {
+        return categoryDtoList.isEmpty();
+    }
+}
---
+++ src/main/java/com/potenday/kreamish/category/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.potenday.kreamish.category.service;
+
+
+import com.potenday.kreamish.category.dto.CategoriesDto;
+
+public interface CategoryService {
+
+    CategoriesDto getAllCategories();
+}
