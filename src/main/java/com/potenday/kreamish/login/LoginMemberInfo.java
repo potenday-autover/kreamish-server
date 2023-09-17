37d128b709c5556fd7c2b5ab2e6abc85750a9b20
kangyunsik <re12io@ajou.ac.kr>
2023-09-17 12:42:26 +0900

    로그인 관련 기능

---
+++ src/main/java/com/potenday/kreamish/login/LoginMemberInfo.java
@@ -0,0 +1,10 @@
+package com.potenday.kreamish.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginMemberInfo {
+    private Long memberId;
+}
---
+++ src/main/java/com/potenday/kreamish/login/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,67 @@
+package com.potenday.kreamish.login.resolver;
+
+import com.potenday.kreamish.common.resolver.CommonArgumentResolver;
+import com.potenday.kreamish.login.Login;
+import com.potenday.kreamish.login.LoginMemberInfo;
+import io.micrometer.common.util.StringUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import java.util.Base64;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+public class LoginMemberArgumentResolver extends CommonArgumentResolver {
+
+    public static final String AUTHORIZATION_PREFIX = "Basic ";
+    public static final String KREAMISH_PASSWORD = "kreamish";
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(Login.class) != null
+            && parameter.getParameterType().equals(LoginMemberInfo.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        // Basic ${base64(memberId:kreamish)}
+        String authString = webRequest.getHeader(AUTHORIZATION);
+        if (StringUtils.isBlank(authString)) {
+            throw new IllegalArgumentException("Authorization header 가 필요합니다.");
+        }
+
+        if (!authString.startsWith(AUTHORIZATION_PREFIX)) {
+            throw new IllegalArgumentException("Authorization prefix 가 올바르지 않습니다");
+        }
+
+        // ${base64(memberId:kreamish)}
+        String encodedStr = authString.substring(AUTHORIZATION_PREFIX.length());
+
+        // memberId:kreamish
+
+        String memberIdAndPassword = new String(
+            Base64.getDecoder().decode(encodedStr.getBytes()));
+
+        // memberId
+        String memberId = memberIdAndPassword.split(":")[0];
+
+        if (isNotLongParsable(memberId)) {
+            throw new IllegalArgumentException("memberId는 1이상의 Long 타입이어야 합니다.");
+        }
+        return new LoginMemberInfo(Long.parseLong(memberId));
+    }
+
+    private Boolean isNotLongParsable(String string) {
+        try {
+            long value = Long.parseLong(string);
+            return value <= 0;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+}
